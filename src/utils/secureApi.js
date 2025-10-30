326d3312e1c2bc1d6accb3c96676dfce823c0e92
venkata nihar billakurthi <84034294+venkataniharbillakurthi@users.noreply.github.com>
2025-10-30 11:40:12 +0530

    Update API base URL to remove port number

--- src/utils/secureApi.js
+++ src/utils/secureApi.js
@@ -1,6 +1,6 @@
 // src/utils/secureApi.js
 
-const API_BASE_URL = 'https://yalladorababu.in:8080/api';
+const API_BASE_URL = 'https://yalladorababu.in/api';
 
 const secureApi = {
   async request(endpoint, options = {}) {
